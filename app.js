8d0b6d5c5325a333b82812712255e92deac484d0
Tyler Watson <tylerww91@gmail.com>
2022-09-22 11:57:47 -0700

    adds notificationDisplay() logic and displays counts of fetched and total database beanies

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const searchForm = document.getElementById('search-form');
 const beanieBabiesList = document.getElementById('beanie-babies-list');
 const beanieImage = document.getElementById('beanie-image');
 const astroSignSelect = document.getElementById('astro-sign-select');
+const notificationDisplay = document.getElementById('notification-display');
 
 /* State */
 let beanies = [];
 let astroSigns = [];
 let error = null;
+let count = 0;
 /* Events */
 window.addEventListener('load', async () => {
     findBeanies();
@@ -37,7 +39,9 @@ async function findBeanies(title, astroSign) {
 
     error = response.error;
     beanies = response.data;
+    count = response.count;
 
+    displayNotifications();
     if (!error) {
         displayBeanieBabies();
     }
@@ -56,6 +60,15 @@ function displayAstroSignOptions() {
     for (const astroSign of astroSigns) {
         const astroSignEl = renderAstroSignOption(astroSign);
         astroSignSelect.append(astroSignEl);
+    }
+}
+
+function displayNotifications() {
+    if (error) {
+        notificationDisplay.classlist.add('error');
+        notificationDisplay.textContent = error.message;
+    } else {
+        notificationDisplay.textContent = `Displaying ${beanies.length} of ${count} matching results`;
     }
 }
 
--- fetch-bb.js
+++ fetch-bb.js
@@ -5,7 +5,11 @@ const SUPABASE_KEY =
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getBeanieBabies(title, astroSign) {
-    let query = client.from('beanie_babies').select('*').order('title').limit(100);
+    let query = client
+        .from('beanie_babies')
+        .select('*', { count: 'exact' })
+        .order('title')
+        .limit(100);
 
     if (title) {
         query = query.ilike('title', `%${title}%`);
